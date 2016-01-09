e4dd185a257fd497eb3d8155e64054a3330a7086
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2016-01-08 19:27:42 -0800

    Initial UserId annotation.

---
+++ src/main/java/com/kryptnostic/annotation/UserId.java
@@ -0,0 +1,19 @@
+package com.kryptnostic.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt; 
+ *
+ * Used to annotate UUIDs as UserIds
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Target({ElementType.PARAMETER,ElementType.LOCAL_VARIABLE,ElementType.FIELD})
+@Documented
+public @interface UserId {
+
+}
